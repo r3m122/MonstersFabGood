a4c03528018ac0441d9348faa2ad16e501835295
r3m122 <71920041+r3m122@users.noreply.github.com>
2022-03-11 12:55:14 +0300

    Add files via upload

---
+++ Bad/Park.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Bad.GameObjects;
+
+namespace Bad
+{
+    /// Класс представляющий локацию
+    class Park
+    {
+        /// Монстры, обитающие на локации
+        private IMonster[] Monsters;
+
+        /// Деревья на локации 
+        private Tree[] trees;
+
+        /// Скамейки на локации
+        private Bench[] benches;
+
+        public Park(IMonster[] Monsters, Tree[] trees, Bench[] benches)
+        {
+            this.Monsters = Monsters;
+            this.Monsters = Monsters;
+            this.Monsters = Monsters;
+        }
+
+
+        public void MonsterInfo()
+        {
+            var littleMonster = Monsters.Where((item) => {
+                return item.ToString() == "Маленький";
+            }).Count();
+
+            Console.WriteLine($"{littleMonster} Маленький");
+
+
+            var middleMonster = Monsters.Where((item) => {
+                return item.ToString() == "Средний";
+            }).Count();
+
+            Console.WriteLine($"{middleMonster} Средний");
+
+
+            var bigMonster = Monsters.Where((item) => {
+                return item.ToString() == "Большой";
+            }).Count();
+
+            Console.WriteLine($"{bigMonster} Большой");
+        }
+
+    }
+}
---
+++ Bad/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Bad.Games;
+
+namespace Bad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IGame[] lvls = new IGame[] {
+                new EzGame(),
+                new MiddleGame(),
+                new HardGame()
+            };
+
+            Console.WriteLine("Выберете уровень сложности и введите его номер:");
+
+            for (int i = 0; i < lvls.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {lvls[i].ToString()}"); // i+1 потому что нормальные люди считают с 1, а не с 0
+            }
+
+            int lvlNumber = Convert.ToInt32(Console.ReadLine()) - 1; // -1 потому что нормальные люди всё ещё считают с 1)))
+
+            if (lvlNumber > -1 && lvlNumber < lvls.Length)
+            {
+                IGame game = lvls[lvlNumber];
+                game.StartGame();
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое число");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
