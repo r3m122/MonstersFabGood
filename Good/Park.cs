11ca6d30f92213d7f2721f58e771af29be496bc7
r3m122 <71920041+r3m122@users.noreply.github.com>
2022-03-11 12:23:30 +0300

    Add files via upload

---
+++ Good/Park.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Good.GameObjects;
+
+namespace Good
+{
+    /// Класс представляющий локацию
+    class Park
+    {
+        /// Монстры, обитающие на локации
+        private IMonster[] Monsters;
+        
+        /// Деревья на локации 
+        private Tree[] trees;
+
+        /// Скамейки на локации
+        private Bench[] benches;
+
+        public Park(IMonster[] Monsters, Tree[] trees, Bench[] benches)
+        {
+            this.Monsters = Monsters;
+            this.Monsters = Monsters;
+            this.Monsters = Monsters;
+        }
+
+        /// Выводит информацию по количеству монстров определённого типа в консоль
+        /// Например:
+        /// 15 Маленький
+        /// 100 Средний
+        /// 55 Большой
+
+        public void MonsterInfo()
+        {
+            var littleMonster = Monsters.Where((item) => {
+                return item.ToString() == "Маленький";
+            }).Count();
+
+            Console.WriteLine($"{littleMonster} Маленький");
+
+
+            var middleMonster = Monsters.Where((item) => {
+                return item.ToString() == "Средний";
+            }).Count();
+
+            Console.WriteLine($"{middleMonster} Средний");
+
+
+            var bigMonster = Monsters.Where((item) => {
+                return item.ToString() == "Большой";
+            }).Count();
+
+            Console.WriteLine($"{bigMonster} Большой");
+        }
+       
+    }
+}
---
+++ Good/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Good.Factories;
+
+namespace Good
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMonsterFactory[] lvls = new IMonsterFactory[] { 
+                new EzLvlMonsterFactory(),
+                new MidleLvlMonsterFactory(),
+                new HardLvlMonsterFactory()
+            };
+
+            Console.WriteLine("Выберете уровень сложности и введите его номер:");
+
+            for (int i = 0; i < lvls.Length; i++) 
+            {
+                Console.WriteLine($"{i+1}. {lvls[i].ToString()}"); // i+1 потому что нормальные люди считают с 1, а не с 0
+            }
+
+            int lvlNumber = Convert.ToInt32(Console.ReadLine()) - 1; // -1 потому что нормальные люди всё ещё считают с 1
+
+            if(lvlNumber > -1 && lvlNumber < lvls.Length) {
+                Game game = new Game(lvls[lvlNumber]); // создаём игру с выбранным уровнем сложности
+                game.StartGame();
+            }
+                else
+            {
+                Console.WriteLine("Недопустимое число");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
